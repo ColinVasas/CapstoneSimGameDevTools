d2e610004aa28144a8b4ecfffdda179593f6943f
Domanik Logan <domaniklogan2002@gmail.com>
2025-01-10 10:43:18 -0800

    added pickup and rotate
    moved the scene to the scene folder, the pickup mechanic is not fully fleshed out, but should be enough for us to start.

---
+++ semiSim/Assets/scripts/pickUpController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpController : MonoBehaviour
+{
+     [Header("Pickup Settings")]
+     [SerializeField] Transform holdArea;
+     private GameObject heldObj;
+     private Rigidbody heldObjRB;
+
+     [Header("Physics Parameters")]
+     [SerializeField] private float pickupRange = 5.0f;
+     [SerializeField] private float pickupForce = 150.0f;
+     [SerializeField] private float rotationSpeed = 100.0f;
+
+     public bool isRotating { get; private set; } = false;
+
+     private void Update()
+     {
+          if (Input.GetMouseButtonDown(0))
+          {
+               if (heldObj == null)
+               {
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                    {
+                         PickupObject(hit.transform.gameObject);
+                    }
+               }
+               else
+               {
+                    DropObject();
+               }
+          }
+
+          if (Input.GetKey(KeyCode.R) && heldObj != null)
+          {
+               isRotating = true;
+               RotateObject();
+          }
+          else
+          {
+               isRotating = false;
+               if (heldObj != null) MoveObject();
+          }
+     }
+
+     void PickupObject(GameObject pickObj)
+     {
+          if (pickObj.GetComponent<Rigidbody>())
+          {
+               heldObjRB = pickObj.GetComponent<Rigidbody>();
+               heldObjRB.useGravity = false;
+               heldObjRB.linearDamping = 10;
+               heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
+
+               heldObjRB.transform.parent = holdArea;
+               heldObj = pickObj;
+          }
+     }
+
+     void DropObject()
+     {
+          heldObjRB.useGravity = true;
+          heldObjRB.linearDamping = 1;
+          heldObjRB.constraints = RigidbodyConstraints.None;
+
+          heldObj.transform.parent = null;
+          heldObj = null;
+     }
+
+     void MoveObject()
+     {
+          if (Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
+          {
+               Vector3 moveDirection = holdArea.position - heldObj.transform.position;
+               heldObjRB.AddForce(moveDirection * pickupForce);
+          }
+     }
+
+     void RotateObject()
+     {
+          float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+          float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+          heldObjRB.angularVelocity = Vector3.zero;
+          heldObj.transform.Rotate(Vector3.up, -mouseX, Space.World);
+          heldObj.transform.Rotate(Vector3.right, mouseY, Space.World);
+     }
+
+     public bool IsRotatingObject()
+     {
+          return isRotating;
+     }
+}
--- semiSim/Assets/scripts/playerMove.cs
+++ semiSim/Assets/scripts/playerMove.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-     public Camera playerCamera;
+     public Camera playerCamera; // Already assigned.
+     public pickUpController pickUpController; // Add this.
+
      public float walkSpeed = 6f;
      public float runSpeed = 12f;
      public float lookSpeed = 2f;
@@ -15,35 +17,39 @@ public class PlayerMove : MonoBehaviour
      private float rotationX = 0;
      private CharacterController characterController;
 
-     private bool canMove = true;
-
-     void Start()
+     private void Start()
      {
           characterController = GetComponent<CharacterController>();
           Cursor.lockState = CursorLockMode.Locked;
           Cursor.visible = false;
+
+          if (pickUpController == null)
+          {
+               Debug.LogError("pickUpController is not assigned in PlayerMove.");
+          }
      }
 
-     void Update()
+     private void Update()
      {
+          if (pickUpController != null && pickUpController.isRotating)
+          {
+               return; // Skip camera movement while rotating an object.
+          }
+
           Vector3 forward = transform.TransformDirection(Vector3.forward);
           Vector3 right = transform.TransformDirection(Vector3.right);
 
           bool isRunning = Input.GetKey(KeyCode.LeftShift);
-          float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-          float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+          float curSpeedX = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical");
+          float curSpeedY = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal");
 
           moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-
           characterController.Move(moveDirection * Time.deltaTime);
 
-          if (canMove)
-          {
-               rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-               rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-               playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-               transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-          }
+          rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+          rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+          playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+          transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
      }
 }
+
