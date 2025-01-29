7a6f16ecd49224b3f9a582f435fa39badefcd9f7
Domanik Logan <domaniklogan2002@gmail.com>
2025-01-29 15:06:19 -0800

    changed things

--- semiSim/Assets/scripts/equipManager.cs
+++ semiSim/Assets/scripts/equipManager.cs
@@ -10,22 +10,19 @@ public class equipManager : MonoBehaviour
 
      private void Start()
      {
-          // Initialize tags: set the first object to "equip" and the second object to have no tag
           firstObject.tag = "equip";
           secondObject.tag = "Untagged";
      }
 
      public void HandleEquip(GameObject equippedObject)
      {
-          // Check if the first object is being equipped
+
           if (equippedObject == firstObject && equippedObject.CompareTag("equip"))
           {
                Debug.Log("First object equipped.");
 
-               // Disable the first object
                equippedObject.SetActive(false);
 
-               // Enable the "equip" tag on the second object
                secondObject.tag = "equip";
                Debug.Log("Second object is now equippable.");
           }
