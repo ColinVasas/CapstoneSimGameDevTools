193c255ddd345201472f60f1685484194fd14466
ColinVasas <colinvasas@gmail.com>
2025-02-01 13:34:48 -0800

    Basic Setup for spincoater
     - Implemented interact with q from player
     - spin coater with basic interact
     - switch statements to easier follow along

---
+++ semiSim/Assets/scripts/InteractController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InteractController : MonoBehaviour
+{
+    [SerializeField] private float pickupRange = 5.0f;
+    pickUpController pickUpController;
+
+
+    void Awake()
+    {
+        pickUpController = GetComponent<pickUpController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+            {
+                Interactable interactable = hit.transform.GetComponent<Interactable>();
+                if(interactable != null)
+                {
+                    interactable.Interact();
+                    // if(pickUpController.heldObj != null)
+                    // {
+                    //     if(pickUpController.heldObj.CompareTag(chuck)) {
+                    //         interactable.ActivateChuck();
+                    //     }
+                    //     if(pickUpController.heldObj.CompareTag(wafer)) {
+                    //         interactable.ActivateWafer();
+                    //     }
+                    // }
+                }
+            }
+        }
+    }
+}
