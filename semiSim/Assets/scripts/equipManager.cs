0deb3616cf59b3cfcc2c8998ff09d3ac883e5b9c
Domanik Logan <domaniklogan2002@gmail.com>
2025-03-01 14:16:19 -0800

    Added the tutorials to the main desktop version

--- semiSim/Assets/scripts/equipManager.cs
+++ semiSim/Assets/scripts/equipManager.cs
@@ -5,8 +5,7 @@ using TMPro;
 
 public class equipManager : MonoBehaviour
 {
-
-     public GameObject hairNet; 
+     public GameObject hairNet;
      public GameObject faceMask;
      public GameObject cleanHood;
      public GameObject goggles;
@@ -15,13 +14,30 @@ public class equipManager : MonoBehaviour
      public GameObject gloves;
 
      public GameObject trigger;
-
-     public TextMeshProUGUI equipText;
-     public float fadeDuration = .5f;
+     public TextMeshProUGUI equipText; // Single TMP UI element for all messages
+     public float fadeDuration = 0.5f;
      public float displayDuration = 1.5f;
+
+     private string[] equipMessages = new string[]
+     {
+        "Face Masks should be in the bin next to the hairnets",
+        "Good Good…. You got a locker assigned to you",
+        "You should find a fresh clean hood, goggles, and “bunny suit”",
+        "Lookin good so far, Last few things, on that bench over there…",
+        "There's a pair of booties, please put them over your shoes",
+        "Lastly, put on some gloves",
+        "Alright…. Are you ready to head in the Clean Room?"
+     };
+
+     private int currentMessageIndex = 0;
 
      private void Start()
      {
+          equipText.gameObject.SetActive(false);
+          Color textColor = equipText.color;
+          textColor.a = 0f;
+          equipText.color = textColor;
+
           hairNet.tag = "equip";
           faceMask.tag = "Untagged";
           cleanHood.tag = "Untagged";
@@ -35,59 +51,59 @@ public class equipManager : MonoBehaviour
 
      public void HandleEquip(GameObject equippedObject)
      {
-          if (!equippedObject.CompareTag("equip")) return; 
+          if (!equippedObject.CompareTag("equip")) return;
 
           switch (equippedObject)
           {
                case var obj when obj == hairNet:
-                    EquipItem(hairNet, faceMask, "HairNet Equipped!");
+                    EquipItem(hairNet, faceMask);
                     break;
-
                case var obj when obj == faceMask:
-                    EquipItem(faceMask, cleanHood, "FaceMask Equipped!");
+                    EquipItem(faceMask, cleanHood);
                     break;
-
                case var obj when obj == cleanHood:
-                    EquipItem(cleanHood, goggles, "CleanHood Equipped!");
+                    EquipItem(cleanHood, goggles);
                     break;
-
                case var obj when obj == goggles:
-                    EquipItem(goggles, bunnySuit, "Goggles Equipped!");
+                    EquipItem(goggles, bunnySuit);
                     break;
-
                case var obj when obj == bunnySuit:
-                    EquipItem(bunnySuit, boots, "BunnySuit Equipped!");
+                    EquipItem(bunnySuit, boots);
                     break;
-
                case var obj when obj == boots:
-                    EquipItem(boots, gloves, "Boots Equipped!");
+                    EquipItem(boots, gloves);
                     break;
-
                case var obj when obj == gloves:
                     trigger.SetActive(true);
-                    EquipItem(gloves, null, "Gloves Equipped!");
+                    EquipItem(gloves, null);
                     break;
-
                default:
                     Debug.Log("Invalid equipment sequence.");
                     break;
           }
      }
 
-     private void EquipItem(GameObject current, GameObject next, string message)
+     private void EquipItem(GameObject current, GameObject next)
      {
           current.SetActive(false);
           if (next != null) next.tag = "equip";
-          StartCoroutine(ShowEquipText(message));
+
+          if (currentMessageIndex < equipMessages.Length)
+          {
+               StartCoroutine(DisplayTextMessage(equipMessages[currentMessageIndex]));
+               currentMessageIndex++;
+          }
      }
 
-     private IEnumerator ShowEquipText(string message)
+     private IEnumerator DisplayTextMessage(string message)
      {
           equipText.text = message;
           equipText.gameObject.SetActive(true);
-          yield return StartCoroutine(FadeText(0f, 1f, fadeDuration));
+
+          yield return StartCoroutine(FadeText(0f, 1f, fadeDuration)); // Fade in
           yield return new WaitForSeconds(displayDuration);
-          yield return StartCoroutine(FadeText(1f, 0f, fadeDuration));
+          yield return StartCoroutine(FadeText(1f, 0f, fadeDuration)); // Fade out
+
           equipText.gameObject.SetActive(false);
      }
 
@@ -104,8 +120,7 @@ public class equipManager : MonoBehaviour
                yield return null;
           }
 
-          color.a = endAlpha; 
+          color.a = endAlpha;
           equipText.color = color;
      }
 }
-
