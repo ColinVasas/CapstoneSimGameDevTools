c188c0251ed1397bc8d0225840c3743c2c6e30f3
Fairgrieve <fairgrievecs@gmail.com>
2025-03-09 22:36:42 -0700

    Implemented tweezers and wafer holder functionality

--- semiSim/Assets/scripts/SnapItem.cs
+++ semiSim/Assets/scripts/SnapItem.cs
@@ -20,11 +20,34 @@ public class SnapItem : MonoBehaviour
     private void OnSelectExited(SelectExitEventArgs args)
     {
         // Check that the exit event is not canceled and that we're in a snap zone
+        Debug.Log("Item released, isInSnapZone: " + isInSnapZone);
         if (!args.isCanceled && isInSnapZone && snapZone != null)
         {
             // Snap the objects pos and rotation to the snap point.
+            Debug.Log("Attempting to snap to: " + snapZone.gameObject.name);
             transform.position = snapZone.snapPoint.position;
             transform.rotation = snapZone.snapPoint.rotation;
+        }
+    }
+    public void TrySnap()
+    {
+        // Check that we're in a snap zone
+        Debug.Log("TrySnap called, isInSnapZone: " + isInSnapZone);
+        if (isInSnapZone && snapZone != null && snapZone.snapPoint != null)
+        {
+            // Snap the objects pos and rotation to the snap point.
+            Debug.Log("Attempting to snap to: " + snapZone.gameObject.name);
+            transform.position = snapZone.snapPoint.position;
+            transform.rotation = snapZone.snapPoint.rotation;
+            
+            // Make the object kinematic so it stays in place
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+            
+            Debug.Log("Snapped to holder");
         }
     }
 }
