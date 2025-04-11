e85dcbbcad23bff54026f5fe4f6582d5dd5e0f71
That Guy <ThatGuyTF3@outlook.com>
2025-04-11 15:45:28 -0700

    Resolve conflict

--- semiSim/Assets/Mini First Person Controller/Scripts/Components/Jump.cs
+++ semiSim/Assets/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -1,5 +1,4 @@
-<<<<<<< ours
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
@@ -33,40 +32,3 @@ public class Jump : MonoBehaviour
         }
     }
 }
-|||||||
-=======
-﻿using UnityEngine;
-
-public class Jump : MonoBehaviour
-{
-    Rigidbody rigidbody;
-    public float jumpStrength = 2;
-    public event System.Action Jumped;
-
-    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
-    GroundCheck groundCheck;
-
-
-    void Reset()
-    {
-        // Try to get groundCheck.
-        groundCheck = GetComponentInChildren<GroundCheck>();
-    }
-
-    void Awake()
-    {
-        // Get rigidbody.
-        rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void LateUpdate()
-    {
-        // Jump when the Jump button is pressed and we are on the ground.
-        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
-        {
-            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
-            Jumped?.Invoke();
-        }
-    }
-}
->>>>>>> theirs
--- semiSim/Assets/Mini First Person Controller/Scripts/Components/Zoom.cs
+++ semiSim/Assets/Mini First Person Controller/Scripts/Components/Zoom.cs
@@ -1,5 +1,4 @@
-<<<<<<< ours
-﻿using UnityEngine;
+using UnityEngine;
 
 [ExecuteInEditMode]
 public class Zoom : MonoBehaviour
@@ -30,37 +29,3 @@ public class Zoom : MonoBehaviour
         camera.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
     }
 }
-|||||||
-=======
-﻿using UnityEngine;
-
-[ExecuteInEditMode]
-public class Zoom : MonoBehaviour
-{
-    Camera camera;
-    public float defaultFOV = 60;
-    public float maxZoomFOV = 15;
-    [Range(0, 1)]
-    public float currentZoom;
-    public float sensitivity = 1;
-
-
-    void Awake()
-    {
-        // Get the camera on this gameObject and the defaultZoom.
-        camera = GetComponent<Camera>();
-        if (camera)
-        {
-            defaultFOV = camera.fieldOfView;
-        }
-    }
-
-    void Update()
-    {
-        // Update the currentZoom and the camera's fieldOfView.
-        currentZoom += Input.mouseScrollDelta.y * sensitivity * .05f;
-        currentZoom = Mathf.Clamp01(currentZoom);
-        camera.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
-    }
-}
->>>>>>> theirs
