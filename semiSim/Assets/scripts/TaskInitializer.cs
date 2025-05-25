a9945705c0c3bdb03ea5140a25276010f81ec808
cstruve <calebstruve6@gmail.com>
2025-05-25 14:29:40 -0700

    Updated Yellow Room Task List UI
    -edited Interactable script
    -created TaskListInitializer script (for yellow room scene)

---
+++ semiSim/Assets/scripts/TaskInitializer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TaskInitializer : MonoBehaviour
+{
+    public TaskListUI taskListUI;
+
+    [SerializeField] private List<string> taskDescriptions = new List<string>
+    {
+        "Open spincoater lid",
+        "Place chuck",
+        "Place cleaned wafer",
+        "Turn on vacuum",
+        "Apply PMMA",
+        "Start spincoater",
+        "Use hotplate and return wafer",
+        "Apply PI",
+        "Final spincoater run",
+        "Head to the wet bench room"
+    };
+
+    void Start()
+    {
+        Debug.Log("TaskInitializer Start called");
+        foreach (string task in taskDescriptions)
+        {
+            Debug.Log("Adding task: " + task);
+            taskListUI?.AddTask(task);
+        }
+    }
+
+    public TaskListUI GetTaskListUI()
+    {
+        return taskListUI;
+    }
+}
